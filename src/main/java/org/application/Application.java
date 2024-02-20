0ac47c191e6fbc17cecf8a9f4c3e1420aa473090
Moses Yi <moses.yjn@gmail.com>
2024-02-19 16:21:50 -0800

    adding comments and fixing menu title displays

--- src/main/java/org/application/Application.java
+++ src/main/java/org/application/Application.java
@@ -5,6 +5,8 @@ import org.menu.Menu;
 public class Application {
     Menu menu = new Menu();
     public void run() {
+
+        //the while loop keeps the user in main menu even if it breaks after the methods run
         while(true){
             menu.mainMenu();
         }
--- src/main/java/org/menu/Menu.java
+++ src/main/java/org/menu/Menu.java
@@ -13,9 +13,13 @@ public class Menu {
     private String[] menuPageTwoList = new String[]{PAGE_ONE, BACK_PAGE};
     private String[] menuPageThreeList = new String[]{PAGE_ONE, BACK_PAGE};
 
+    //--------------------------------- menu creation -------------------------------------
+
+    //this method creates a menu for the user
     public void menu(String[] string_List) {
         int number = 1;
-        System.out.println("Main Menu");
+
+        //the for loop creates a menu with a number and a string
         for (String s : string_List) {
             System.out.println(number + ": " + s);
             number++;
@@ -23,97 +27,186 @@ public class Menu {
         System.out.println("Please select a number: ");
     }
 
+    // --------------------------------- main menu method ----------------------------------
+
+    //this method is for the main menu selection for the user
     public void mainMenu() {
 
+        //prints main menu display
+        mainMenuDisplay();
+
+        //prints out main menu
         menu(mainMenuList);
+
+        //gets the user input for menu selection
         String selection = userInput();
 
+        //needs a while loop for breaks;
         while(!selection.isBlank()) {
 
             if (selection.equals("1")) {
+
+                //user enters to menu selection 1
                 mainMenu_Page1(selection);
                 break;
             }
             else if (selection.equals("2")) {
+
+                //user enters to menu selection 2
                 mainMenu_Page2(selection);
                 break;
             }
             else if (selection.equals("3")) {
+
+                //user enters to menu selection 3
                 mainMenu_Page3(selection);
                 break;
             }
             else if (selection.equals("4")) {
+                //user exits the menu and application
                 System.out.println("You are now exiting out of the application");
                 System.exit(0);
             }
             else {
+                //prevents user to break the selection menus
                 System.out.println("Invalid option");
                 break;
             }
         }
     }
 
+    //------------------------------------ page menu methods ----------------------------------
+
     public void mainMenu_Page1(String selection) {
+
+        //the while loop keeps the user in page 1 menu
         while(selection.equals("1")) {
 
+            //prints page 1 menu display
+            page1MenuDisplay();
+
+            //creates a menu for page 1 for the user to select from
             menu(menuPageOneList);
+
+            //gets the user input for menu selection
             String page1_Selection = userInput();
 
+            //the if statement checks to see if user selects one of the options
             if (page1_Selection.equals("1")) {
+
+                //user enters menu selection 1
                 System.out.println("You are in menu page one");
             }
             else if (page1_Selection.equals("2")) {
+
+                //user goes back to the main menu
                 System.out.println("You are going back to the main menu");
                 break;
             }
             else {
+
+                //prevents user to break the selection menus
                 System.out.println("Invalid input");
             }
         }
     }
 
     public void mainMenu_Page2(String selection) {
+
+        //the while loop keeps the user in page 2 menu
         while(selection.equals("2")) {
 
+            //prints page 2 menu display
+            page2MenuDisplay();
+
+            //creates a menu for page 2 for the user to select from
             menu(menuPageTwoList);
+
+            //gets the user input for menu selection
             String page2_Selection = userInput();
 
             if (page2_Selection.equals("1")) {
+
+                //user enters menu selection 1
                 System.out.println("You are in menu page one");
             }
             else if (page2_Selection.equals("2")) {
+
+                //user goes back to the main menu
                 System.out.println("You are returning to the main menu");
                 break;
             }
             else {
+
+                //prevents user to break the selection menus
                 System.out.println("Invalid input");
             }
         }
     }
 
     public void mainMenu_Page3(String selection) {
+
+        //the while loop keeps the user in page 3 menu
         while(selection.equals("3")) {
 
+            //prints page 3 menu display
+            page3MenuDisplay();
+
+            //creates a menu for page 3 for the user to select from
             menu(menuPageThreeList);
+
+            //gets the user input for menu selection
             String page3_Selection = userInput();
 
             if (page3_Selection.equals("1")) {
+
+                //user enters menu selection 1
                 System.out.println("You are in menu page one");
             }
             else if (page3_Selection.equals("2")) {
+
+                //user goes back to the main menu
                 System.out.println("You are returning to the main menu");
                 break;
             }
             else {
+
+                //prevents user to break the selection menus
                 System.out.println("Invalid input");
             }
         }
     }
 
+    //--------------------------- method for user inputs ---------------------------------------
+
     public String userInput(){
+
+        //user enters an input for menu selection
         Scanner userInput = new Scanner(System.in);
         String selection = userInput.nextLine();
+
+        //returns the inputted string
         return selection;
     }
+
+
+    // -------------------------- Displays with sout methods -----------------------------------
+
+    public void mainMenuDisplay() {
+        System.out.println("Main Menu");
+    }
+
+    public void page1MenuDisplay() {
+        System.out.println("Page One Menu");
+    }
+
+    public void page2MenuDisplay() {
+        System.out.println("Page Two Menu");
+    }
+
+    public void page3MenuDisplay() {
+        System.out.println("Page Three Menu");
+    }
+
 
 }
